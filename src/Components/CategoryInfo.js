7b2eb1e959f6e53a9b331ee15cb69fbcca1f1dcf
jayeshbhavsar03 <bhavsarjayesh9370@gmail.com>
2022-04-18 12:33:39 +0530

    Initial commit

---
+++ src/Components/CategoryInfo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const CategoryInfo = ({country,category}) => {
+
+    
+  return (
+      <>
+    {/* <h2 className="categoryinfo" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>{category.toUpperCase()} NEWS - {
+        <span style={{color:"#922a44",lineHeight:"25px"}}>
+            &nbsp;
+           <img src={flagUrl} alt="" style={{width:"45px",lineHeight:"20px"}} className="flag"/>
+        </span>
+     }</h2> */}
+     <h5 className="categoryinfo" style={{marginTop:"20px",textAlign:"center"}}>NEWSAPP MADE BY - SHUBHAM & SANKALP 😎</h5>
+     </>
+  )
+}
+
+export default CategoryInfo
